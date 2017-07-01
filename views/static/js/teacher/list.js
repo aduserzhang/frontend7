e39b524a08ac00a21c052352dfd8a708fccdde5d
zwl <1234@qw>
2017-07-01 15:57:45 +0800

    注销按钮完成

--- views/static/js/teacher/list.js
+++ views/static/js/teacher/list.js
@@ -21,13 +21,45 @@ define(["jquery","template","bootstrap"],function($,template){
 						tc_id:tcid
 					},
 					success:function(data){
-						console.log(data);
+						// console.log(data);
 						if(data.code == 200){
 						//将数据渲染到模态框找中
 						var html = template("tc-check-tpl",data.result);
 						$("#teacherModal").html(html);
 						//显示模态框
 						$("#teacherModal").modal("show");
+						}
+					}
+				})
+		});
+
+		// 将注销和启用按钮注册点击事件
+		$("#teacherlist").on("click",".onoff",function(){
+				var tcid = $(this).parent().data("id");
+				var status = $(this).data("status");
+				var $that = $(this)
+				
+				$.ajax({
+					url:"/api/teacher/handle",
+					type:"post",
+					data:{
+						tc_id:tcid,
+						tc_status:status
+					},
+					success:function(data){
+						if(data.code == 200){
+							console.log(data);
+							$that.data("status",data.result.tc_status);
+							//如果后台数据正常返回数据，页面上的按钮也要变化
+							if(data.result.tc_status == 1){
+									$that.removeClass("btn-warning");
+									$that.addClass("btn-success");
+									$that.text("启 用");
+							}else{
+								  $that.removeClass("btn-success");
+									$that.addClass("btn-warning");
+									$that.text("注 销");
+							}
 						}
 					}
 				})
