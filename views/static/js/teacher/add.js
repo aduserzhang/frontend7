6ac0c926f325daf68844c7639d30651bb865406e
zwl <1234@qw>
2017-07-02 21:43:53 +0800

    添加和编辑讲师 时间和表单验证功能也完成了

--- views/static/js/teacher/add.js
+++ views/static/js/teacher/add.js
@@ -1,4 +1,4 @@
-define(["jquery","template","util","form"],function($,template,util){
+define(["jquery","template","util","form","datepicker","datepicker.zh","validate"],function($,template,util){
 		$(function(){
 
 			//用地址栏上的ID来区别当前是进行编辑讲师信息还是添加讲师信息
@@ -19,9 +19,68 @@ define(["jquery","template","util","form"],function($,template,util){
 							data.result.btnText = "保 存";
 							// 将保存的类型设为 type = "edit"
 							data.result.type = "edit";
-
 							var strTpl = template("operate-tpl",data.result);
-							$(".teacher").html(strTpl);						
+							$(".teacher").html(strTpl);	
+							// 时间日历插件
+							$("input[name=tc_join_date]").datepicker({
+									format:"yyyy-mm-dd",
+									language:"zh-CN"
+							});	
+							//表单验证插件
+							$("#teacherform").validate({
+								//为当前表单项提供验证信息
+								description:{
+									"tcname":{
+										required:"请输入用户名!",
+									},
+									"tcpass":{
+										required:"请输入密码!"
+									},
+									"tcjoindata":{
+										required:"请输入入职时间"
+									}
+								},
+								onBlur:true,
+								onKeyup:true,
+								// 当前表单元素验证通过后，是否自动提交表单 默认是true
+								sendForm:false,
+								// 当前表单元素没有通过校验 调用
+								eachInvalidField:function(){
+									// 当前元素校验没有通过，设置错误的提示
+									this.parent().parent().addClass("has-error").removeClass("has-success");
+									this.parent().next().removeClass("hide");
+								},
+								// 当前元素通过验证，调用
+								eachValidField:function(){
+									// 设置校验通过样式
+									this.parent().parent().addClass("has-success").removeClass("has-error");
+								},
+								// 当表单内所有的元素都验证通过，调用
+								valid:function(){
+									console.log("验证通过了");
+									// 验证通过，提交表单
+									 	var type = $("#btnSave").data("type");
+									  var url = "";
+									  // 通过获取按钮的类型 判断提交是编辑 还是 保存 接口
+									  if(type == "edit"){
+									  	url = "/api/teacher/update";
+									  }else{
+									  	url = "/api/teacher/add";
+									  }
+									  //使用jQuery.form插件将表单进行一步提交
+									  $("#teacherform").ajaxSubmit({
+									  	url:url,
+									  	type:"post",
+									  	success:function(data){
+									  		console.log(456);
+									  		if(data.code == 200){
+									  			console.log(data);
+									  			location.href = "/teacher/list";
+									  		}
+									  	}
+									  })
+								}
+				      });			
 						}
 					}
 				})
@@ -35,31 +94,73 @@ define(["jquery","template","util","form"],function($,template,util){
 					type:"add"
 				});
 				$(".teacher").html(html);
+				// 时间日历插件
+				$("input[name=tc_join_date]").datepicker({
+						format:"yyyy-mm-dd",
+						language:"zh-CN"
+				});
+				// 表单验证插件
+				$("#teacherform").validate({
+					//为当前表单项提供验证信息
+					description:{
+						"tcname":{
+							required:"请输入用户名!",
+						},
+						"tcpass":{
+							required:"请输入密码!"
+						},
+						"tcjoindata":{
+							required:"请输入入职时间"
+						}
+					},
+					onBlur:true,
+					onKeyup:true,
+					// 当前表单元素验证通过后，是否自动提交表单 默认是true
+					sendForm:false,
+					// 当前表单元素没有通过校验 调用
+					eachInvalidField:function(){
+						// 当前元素校验没有通过，设置错误的提示
+						this.parent().parent().addClass("has-error").removeClass("has-success");
+						this.parent().next().removeClass("hide");
+					},
+					// 当前元素通过验证，调用
+					eachValidField:function(){
+						// 设置校验通过样式
+						this.parent().parent().addClass("has-success").removeClass("has-error");
+					},
+					// 当表单内所有的元素都验证通过，调用
+					valid:function(){
+						console.log("验证通过了");
+						// 验证通过，提交表单
+						 	var type = $("#btnSave").data("type");
+						  var url = "";
+						  // 通过获取按钮的类型 判断提交是编辑 还是 保存 接口
+						  if(type == "edit"){
+						  	url = "/api/teacher/update";
+						  }else{
+						  	url = "/api/teacher/add";
+						  }
+						  //使用jQuery.form插件将表单进行一步提交
+						  $("#teacherform").ajaxSubmit({
+						  	url:url,
+						  	type:"post",
+						  	success:function(data){
+						  		console.log(456);
+						  		if(data.code == 200){
+						  			console.log(data);
+						  			location.href = "/teacher/list";
+						  		}
+						  	}
+						  })
+					}
+				});
 			}
 
 			// 给保存按钮添加事件
-			$(".teacher").on("click","#btnSave",function(){
-				  var type = $(this).data("type");
-				  var url = "";
-				  // 通过获取按钮的类型 判断提交是编辑 还是 保存 接口
-				  if(type == "edit"){
-				  	url = "/api/teacher/update";
-				  }else{
-				  	url = "/api/teacher/add";
-
-				  }
-				  //使用jQuery.form插件将表单进行一步提交
-				  $("#teacherform").ajaxSubmit({
-				  	url:url,
-				  	type:"post",
-				  	success:function(data){
-				  		if(data.code == 200){
-				  			location.href = "/teacher/list";
-				  		}
-				  	}
-				  })
-				  return false;
-			})
+			// $(".teacher").on("click","#btnSave",function(){
+				 
+			// 	  return false;
+			// })
 			 
 		});
 })
--- views/static/libs/config.js
+++ views/static/libs/config.js
@@ -7,11 +7,20 @@ require.config({
      	"template": "assets/artTemplate/template",
      	"bootstrap": "assets/bootstrap/js/bootstrap.min",
      	"util":"static/js/util",
-          
+          "datepicker":"assets/bootstrap-datepicker/js/bootstrap-datepicker.min",
+          "datepicker.zh":"assets/bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN.min",
+          "validate":"assets/jquery-validate/jquery-validate.min"
+
      },
      shim:{
      	"bootstrap":{
      		deps:["jquery"]
-     	}
+     	},
+          "datepicker.zh":{
+               deps:["datepicker"]
+          },
+          "validate":{
+               deps:["jquery"]
+          }
      }
 });
